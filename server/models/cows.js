52c4751b7ab4ddf26d0c3c02688b97a5c294a6e7
Denis Sanches <denis.a.sanches@gmail.com>
2020-09-13 12:53:15 -0400

    implement create method

--- server/models/cows.js
+++ server/models/cows.js
@@ -5,14 +5,24 @@ var mysql = require('mysql');
 
 module.exports = {
   getAll: function (callback) {
-      var queryString = 'SELECT * FROM list_of_cows'
-      db.connection.query(queryString, (err, data) => {
-          if (err) {
-              throw 'Error getting cows'
-          } else {
-              callback(err, data)
-          }
-      })
+    let queryString = 'SELECT * FROM list_of_cows;';
+    db.connection.query(queryString, (err, data) => {
+      if (err) {
+        throw 'Error getting cows';
+      } else {
+        callback(err, data);
+      }
+    });
   },
-  create: function (callback) {},
+  create: function ({ name, description }, callback) {
+    let queryString = `INSERT INTO list_of_cows (name, description) \ 
+    VALUES (${mysql.escape(name)}, ${mysql.escape(description)});`;
+    db.connection.query(queryString, (err, data) => {
+      if (err) {
+        throw 'Error adding cows to herd';
+      } else {
+        callback(err, data);
+      }
+    });
+  },
 };
