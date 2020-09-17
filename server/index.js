a532592f25ad3b2209db105200f9c7b63bf9de18
Denis Sanches <denis.a.sanches@gmail.com>
2020-09-17 19:08:08 -0400

    add working delete and put routes

--- server/index.js
+++ server/index.js
@@ -1,35 +1,65 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 const models = require('./models');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(express.static('./client/dist'));
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
 
 app.get('/api/cows', (req, res) => {
   models.cows.getAll((err, data) => {
     if (err) {
       res.sendStatus(500);
     } else {
-      console.log(data);
+      console.log('GET', data);
       res.status(200).json(data);
     }
   });
 });
 
 app.post('/api/cows', (req, res) => {
-    console.log('Adding cows to herd', req.body)
   models.cows.create(req.body, (err, data) => {
     if (err) {
       res.sendStatus(500);
     } else {
-      console.log(data);
+      console.log('POST', data);
       res.status(201).json(data);
     }
   });
 });
+
+app.delete('/api/cows/:id', (req, res) => {
+  console.log('DELETE Request', req.params.id);
+  models.cows.delete(req.params.id, (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      console.log('DELETE', data);
+      res.status(200).json(data);
+    }
+  });
+});
+
+app.put('/api/cows/', (req, res) => {
+  models.cows.update(req.body, (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      console.log('UPDATE', data);
+      res.status(200).json(data);
+    }
+  });
+});
+
+app.options('*', cors());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- server/models/cows.js
+++ server/models/cows.js
@@ -5,7 +5,9 @@ var mysql = require('mysql');
 
 module.exports = {
   getAll: function (callback) {
+    //
     let queryString = 'SELECT * FROM list_of_cows;';
+    //
     db.connection.query(queryString, (err, data) => {
       if (err) {
         throw 'Error getting cows';
@@ -15,11 +17,37 @@ module.exports = {
     });
   },
   create: function ({ name, description }, callback) {
+    //
     let queryString = `INSERT INTO list_of_cows (name, description) \ 
     VALUES (${mysql.escape(name)}, ${mysql.escape(description)});`;
+    //
     db.connection.query(queryString, (err, data) => {
       if (err) {
         throw 'Error adding cows to herd';
+      } else {
+        callback(err, data);
+      }
+    });
+  },
+  delete: function (id, callback) {
+    //
+    let queryString = `DELETE FROM list_of_cows WHERE list_of_cows.id = ${mysql.escape(id)}`;
+    //
+    db.connection.query(queryString, (err, data) => {
+      if (err) {
+        throw 'Error deleting cow';
+      } else {
+        callback(err, data);
+      }
+    });
+  },
+  put: function ({ id, name, description }, callback) {
+    //
+    let queryString = `UPDATE list_of_cows SET ? WHERE list_of_cows.id = ${mysql.escape(id)}`;
+    //
+    db.connection.query(queryString, { name, description }, (err, data) => {
+      if (err) {
+        throw 'Error updating row';
       } else {
         callback(err, data);
       }
